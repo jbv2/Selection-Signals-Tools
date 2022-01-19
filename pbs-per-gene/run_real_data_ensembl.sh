29cc81b10880b4bb9746ed95d5c43d8961fc7f4d
judith <judith.vballesteros@gmail.com>
2022-01-18 19:04:10 -0600

    changing launcher

--- pbs-per-gene/run_real_data_ensembl.sh
+++ pbs-per-gene/run_real_data_ensembl.sh
@@ -1,15 +1,15 @@
 echo -e "======\n NF execution \n======" \
 && rm -rf real-data/results/ensembl \
 && nextflow run pbs_per_gene.nf \
-	--vcffile "real-data/data/MXB_1KG_IBS_CHB_aa_derived.vcf.gz" \
+	--vcffile "real-data/data/MXB_1KG_IBS_CHB_aa_derived_95NatAm.vcf.gz" \
 	--ref_gene "real-data/reference/mart_export_hg19.txt" \
-	--pop_outgroup "real-data/reference/pop_chb" \
-	--pop_ingroup "real-data/reference/pop_ibs" \
-	--pop_target "real-data/reference/pop_mxb" \
-  --pop_1 "mxb_chb" \
-  --pop_2 "mxb_ibs" \
+	--pop_ingroup "real-data/reference/pop_chb" \
+	--pop_outgroup "real-data/reference/pop_ibs" \
+	--pop_target "real-data/reference/pop90_mxb" \
+  --pop_2 "mxb_chb" \
+  --pop_1 "mxb_ibs" \
   --pop_3 "ibs_chb" \
-  --output_dir real-data/results/ensembl \
+  --output_dir real-data/results95/ensembl \
 	-resume \
 	-with-report real-data/results/`date +%Y%m%d_%H%M%S`_report.html \
 	-with-dag real-data/results/`date +%Y%m%d_%H%M%S`.DAG.html \
