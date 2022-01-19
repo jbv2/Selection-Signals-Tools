164c6dc7cf1643bdd45a85a0419e7f023272974e
jbv2 <judith.vballesteros@gmail.com>
2022-01-18 19:12:30 -0600

    updating launchers

--- pbs-per-gene/run_real_data_genehancer.sh
+++ pbs-per-gene/run_real_data_genehancer.sh
@@ -1,16 +1,16 @@
 echo -e "======\n NF execution \n======" \
-&& rm -rf real-data/results/genehncer \
+&& rm -rf real-data/results/ensembl \
 && nextflow run pbs_per_gene.nf \
-	--vcffile "real-data/data/MXB_1KG_IBS_CHB_aa_derived.vcf.gz" \
-	--ref_gene "real-data/reference/genehancer_export.tsv"  \
-	--pop_outgroup "real-data/reference/pop_chb" \
-	--pop_ingroup "real-data/reference/pop_ibs" \
-	--pop_target "real-data/reference/pop_mxb" \
-  --pop_1 "mx_chb" \
-  --pop_2 "mx_ibs" \
+	--vcffile "real-data/data/MXB_1KG_IBS_CHB_aa_derived_95NatAm.vcf.gz" \
+	--ref_gene "real-data/reference/genehancer_export.tsv" \
+	--pop_ingroup "real-data/reference/pop_chb" \
+	--pop_outgroup "real-data/reference/pop_ibs" \
+	--pop_target "real-data/reference/pop90_mxb" \
+  --pop_2 "mxb_chb" \
+  --pop_1 "mxb_ibs" \
   --pop_3 "ibs_chb" \
-  --output_dir real-data/results/genehancer \
+  --output_dir real-data/results95/genehancer \
 	-resume \
-	-with-report test/results/`date +%Y%m%d_%H%M%S`_report.html \
-	-with-dag test/results/`date +%Y%m%d_%H%M%S`.DAG.html \
+	-with-report real-data/results/`date +%Y%m%d_%H%M%S`_report.html \
+	-with-dag real-data/results/`date +%Y%m%d_%H%M%S`.DAG.html \
 && echo -e "======\n MXB Population Branch Statistics per feature calculator: Pipeline SUCCESSFUL \n======"
