2da2565f97a04597a0e8684ba0201fa36b47d02d
jbv2 <judith.vballesteros@gmail.com>
2022-02-24 13:26:09 -0600

    real data genehancer test

--- pbs-per-gene/run_real_data_genehancer.sh
+++ pbs-per-gene/run_real_data_genehancer.sh
@@ -1,14 +1,16 @@
 echo -e "======\n NF execution \n======" \
 && nextflow run pbs_per_gene.nf \
-	--vcffile "real-data/data/MXB_1KG_IBS_CHB_aa_derived_95NatAm.vcf.gz" \
+	--vcffile "real-data/data/Central_Gulf_CHB_aa_derived_90NatAm.vcf.gz" \
 	--ref_gene "real-data/reference/genehancer_export.tsv" \
-	--pop_ingroup "real-data/reference/pop_chb" \
-	--pop_outgroup "real-data/reference/pop_ibs" \
-	--pop_target "real-data/reference/pop95_mxb" \
-  --pop_2 "mxb_chb" \
-  --pop_1 "mxb_ibs" \
-  --pop_3 "ibs_chb" \
-  --output_dir real-data/results95/genehancer \
+	--ref_gene_A "real-data/reference/genehancer_export_A.tsv" \
+	--ref_gene_B "real-data/reference/genehancer_export_B.tsv" \
+	--pop_ingroup "real-data/reference/pop90_gulf" \
+	--pop_outgroup "real-data/reference/pop_chb" \
+	--pop_target "real-data/reference/pop90_central" \
+  --pop_2 "central_gulf" \
+  --pop_1 "central_chb" \
+  --pop_3 "gulf_chb" \
+  --output_dir real-data/results90/genehancer/central_gulf \
 	-resume \
 	-with-report real-data/results/`date +%Y%m%d_%H%M%S`_report.html \
 	-with-dag real-data/results/`date +%Y%m%d_%H%M%S`.DAG.html \
