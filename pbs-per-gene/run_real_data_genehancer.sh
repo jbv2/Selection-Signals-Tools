144295a3d970f6ea2e2f60b0931bad6dd44b1235
judith <judith.vballesteros@gmail.com>
2022-03-10 11:37:36 -0600

    Updating

--- pbs-per-gene/run_real_data_genehancer.sh
+++ pbs-per-gene/run_real_data_genehancer.sh
@@ -1,14 +1,14 @@
 echo -e "======\n NF execution \n======" \
 && nextflow run pbs_per_gene.nf \
-	--vcffile "real-data/data/Central_Gulf_CHB_aa_derived_90NatAm.vcf.gz" \
+	--vcffile "real-data/data/Central_Mayan_CHB_aa_derived_90NatAm.vcf.gz" \
 	--ref_gene "real-data/reference/genehancer_export.tsv" \
-	--pop_ingroup "real-data/reference/pop90_gulf" \
+	--pop_ingroup "real-data/reference/pop90_mayan" \
 	--pop_outgroup "real-data/reference/pop_chb" \
 	--pop_target "real-data/reference/pop90_central" \
-  --pop_2 "central_gulf" \
+  --pop_2 "central_mayan" \
   --pop_1 "central_chb" \
-  --pop_3 "gulf_chb" \
-  --output_dir real-data/results90/genehancer/central_gulf \
+  --pop_3 "mayan_chb" \
+  --output_dir real-data/results90/genehancer/central_mayan \
 	-resume \
 	-with-report real-data/results/`date +%Y%m%d_%H%M%S`_report.html \
 	-with-dag real-data/results/`date +%Y%m%d_%H%M%S`.DAG.html \
